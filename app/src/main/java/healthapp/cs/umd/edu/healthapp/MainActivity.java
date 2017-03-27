36526f60d9a0dfca04a6d6d174543d4f7e708653
Clayton Zheng <czheng9001@gmail.com>
2017-03-26 20:34:20 -0400

    Initial commit

---
+++ app/src/main/java/healthapp/cs/umd/edu/healthapp/MainActivity.java
@@ -0,0 +1,14 @@
+package healthapp.cs.umd.edu.healthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("lol");
+    }
+}
